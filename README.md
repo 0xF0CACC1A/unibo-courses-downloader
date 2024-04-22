7334592939e47e64969755b4fe83a29f547d9890
0xF0CACC1A <m.manuelli@proton.me>
2024-04-22 19:12:57 +0200

    download slides too; code refactoring

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 unibo courses downloader
 ============================
 
-`./main.py cookies.json [folderID]`
+`./main.py panopto cookies.json [folderID]`
 
-TUTORIAL
+TUTORIAL PANOPTO
 -----
 *   assicurati di essere iscritto al "virtuale" del corso del quale vuoi scaricare le registrazioni.
 *   (da, per esempio, il "virtuale" del corso) apri il link nella tab corrente di una registrazione qualsiasi che vuoi scaricare.
@@ -12,3 +12,11 @@ TUTORIAL
 *   dalla barra degli indirizzi, estrai il valore del campo "folderID" (e.g. se https://unibo.cloud.panopto.eu/Panopto/Pages/Sessions/List.aspx#folderID="8cea2670-08a2-4f20-a11d-afb200e2910f" => "8cea2670-08a2-4f20-a11d-afb200e2910f" ).
 *   hai tutti gli ingredienti per poter eseguire il programma ;)
  
+`./main.py virtuale cookies.json [folderID]`
+
+TUTORIAL VIRTUALE
+-----
+*   assicurati di essere iscritto al "virtuale" del corso dal quale vuoi scaricare le slide.
+*   (da, per esempio, il "virtuale" del corso) esporta i cookie in formato json usando questa [estensione open-source](https://github.com/kairi003/Get-cookies.txt-Locally).
+*   dalla barra degli indirizzi, estrai il valore del campo "id" (e.g. se https://virtuale.unibo.it/course/view.php?id=39625 => "39625" ).
+*   hai tutti gli ingredienti per poter eseguire il programma ;)
--- main.py
+++ main.py
@@ -1,44 +1,75 @@
 #!/usr/bin/env python3
-import requests
-import json
-import sys
-import re
-from yt_dlp import YoutubeDL
+import json, os, re, requests, sys
 from threading import Thread
 
+def set_session_cookies(s: requests.Session, cookies: list[dict[str, str]]):
+    # https://stackoverflow.com/questions/17224054/how-to-add-a-cookie-to-the-cookiejar-in-python-requests-library
+    # https://www.reddit.com/r/Python/comments/6n7l79/loading_cookies_in_requests_module/
+    # set cookie jar with essential std values
+    for cookie in cookies:
+        cookie = {
+            "name": cookie['name'],
+            "value": cookie['value'],
+            "domain": cookie['domain'],
+        }
+        s.cookies.set(**cookie)
+
 def download_video(url : str, out: str):
+    from yt_dlp import YoutubeDL
     with YoutubeDL({"quiet": True, 'outtmpl': f'{out}'}) as ydl: ydl.download(url)
 
-with open(sys.argv[1]) as f:
-    cookies = json.load(f)
+def download_recordings(cookies: list[dict[str, str]], folderID : str):
+    s = requests.Session()
+    set_session_cookies(s,cookies)
 
-s = requests.Session()
+    url = f'https://unibo.cloud.panopto.eu/Panopto/Pages/Sessions/List.aspx#folderID="{folderID}"&page=0&maxResults=250'
+    s.get(url)
 
-# https://stackoverflow.com/questions/17224054/how-to-add-a-cookie-to-the-cookiejar-in-python-requests-library
-# https://www.reddit.com/r/Python/comments/6n7l79/loading_cookies_in_requests_module/
-# set cookie jar with essential std values
-for cookie in cookies:
-    cookie = {
-        "name": cookie['name'],
-        "value": cookie['value'],
-        "domain": cookie['domain'],
-    }
-    s.cookies.set(**cookie)
+    # load payload template for next req
+    with open("payload.json") as f:
+        payload = json.load(f)
+    payload["queryParameters"]["folderID"] = folderID # TODO archived videos
 
-folderID = sys.argv[2]
-url = f'https://unibo.cloud.panopto.eu/Panopto/Pages/Sessions/List.aspx#folderID="{folderID}"&page=0&maxResults=250'
-s.get(url)
+    url = 'https://unibo.cloud.panopto.eu/Panopto/Services/Data.svc/GetSessions'
+    r = s.post(url, json=payload)
 
-# load payload template for next req
-with open("payload.json") as f:
-    payload = json.load(f)
-payload["queryParameters"]["folderID"] = folderID
+    regex = r'"FolderName":"(.*?)".*?"IosVideoUrl":"(https.*?[mp4|m3u8])"'
+    for (i, m) in enumerate(re.finditer(regex, r.text)):
+        folderName = m.group(1).replace("\\/", " ")
+        url = m.group(2).replace("\\", "")
+        Thread(target=download_video, args=[url, f"{folderName}/{str(i)}.mp4"]).start()
 
-url = 'https://unibo.cloud.panopto.eu/Panopto/Services/Data.svc/GetSessions'
-r = s.post(url, json=payload)
+def download_file(s: requests.Session, url: str, name: str):
+    r = s.get(url)    
+    with open(name,'wb+') as f: f.write(r.content) # + => create file
+    print(f"scaricato {name}!")
 
-regex = r'"FolderName":"(.*?)".*?"IosVideoUrl":"(https.*?[mp4|m3u8])"'
-for (i, m) in enumerate(re.finditer(regex, r.text)):
-    folderName = m.group(1).replace("\\/", " ")
-    url = m.group(2).replace("\\", "")
-    Thread(target=download_video, args=[url, f"{folderName}/{str(i)}.mp4"]).start()
+def download_slides(cookies: list[dict[str, str]], id: str):
+    s = requests.Session()
+    set_session_cookies(s,cookies)
+    
+    r = s.get(f"https://virtuale.unibo.it/course/view.php?id={id}")
+    regex = r'<a href="(https:\/\/virtuale\.unibo\.it\/mod\/{0}\/view.php\?id=([0-9]+))[\s\S]*?<h5 class="mb-0">([\s\S]*?)<\/h5>[\s\S]*?<\/a>'
+    slides = [(m.group(1), m.group(3).strip()) for m in re.finditer(regex.format("resource"), r.text)]
+    # urls = [(m.group(1), m.group(3).strip()) for m in re.finditer(regex.format("url"), r.text)]
+    folders = [(f"https://virtuale.unibo.it/mod/folder/download_folder.php?id={m.group(2)}", m.group(3).strip()) for m in re.finditer(regex.format("folder"), r.text)]
+
+    out_dir = id # TODO get course's name
+    os.mkdir(out_dir) if not os.path.exists(out_dir) else exit(f"{out_dir} already existed!")
+    # for url in urls:
+    #     r = s.get(url[0])
+    #     regex = r'<div class="urlworkaround">.*?<a href="(.*?)">.*?<\/a><\/div>'
+    #     res = re.search(regex, r.text)
+    #     if res == None: raise Exception(f"no match found\nurl: {url}\nregex: {regex}")
+    #     Thread(target=download_file, args=[s, res.group(1), f"{out_dir}/{url[1].replace('/', '-')}.pdf"]).start()
+    for f in folders:
+        Thread(target=download_file, args=[s, f[0], f"{out_dir}/{f[1].replace('/', '-')}.zip"]).start()
+    for f in slides:
+        Thread(target=download_file, args=[s, f[0], f"{out_dir}/{f[1].replace('/', '-')}.pdf"]).start()
+
+def main():
+    with open(sys.argv[2]) as f: cookies = json.load(f)
+    download_recordings(cookies, sys.argv[3]) if sys.argv[1] == "panopto" else download_slides(cookies, sys.argv[3])
+
+if __name__ == "__main__":
+    main()
